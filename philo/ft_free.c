a3340e3d90b5f35e689b269bbb05d5d80db34e27
sielee <tldms975@naver.com>
2022-10-15 01:05:46 +0900

    mod ft_free

--- philo/ft_free.c
+++ philo/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 22:34:02 by sielee            #+#    #+#             */
-/*   Updated: 2022/08/28 15:38:35 by sielee           ###   ########seoul.kr  */
+/*   Updated: 2022/10/15 01:05:26 by sielee           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_free(void **ptr)
 {
-	if (*ptr && ptr)
+	if (ptr && *ptr)
 	{
 		free(*ptr);
 		*ptr = NULL;
